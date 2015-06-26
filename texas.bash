vifon/texas

commit da596f207958f8d8aff507c96157d8ad2d34ab27

texas started in an existing tmux session will never close the shell that started it

texas.bash
@@ -12,7 +12,10 @@ texas()
         return
     fi
 
-    TEXAS_RANGER_PID=$(tmux split-window -p 70 -P -F '#{pane_pid}' "TEXAS_SHELL_PID=$$ ranger")
+    TEXAS_RANGER_PID=$(tmux split-window -p 70 -P -F '#{pane_pid}' "LAUNCH_TEXAS=$LAUNCH_TEXAS TEXAS_SHELL_PID=$$ ranger")
+
+    # Unset the variable only here because the ranger plugin reacts to it.
+    unset LAUNCH_TEXAS
 
     cd()
     {
@@ -42,6 +45,5 @@ texas()
 }
 
 if [ -n "$LAUNCH_TEXAS" ]; then
-    unset LAUNCH_TEXAS
     texas
 fi

texas.zsh
@@ -6,7 +6,10 @@ if [ -z "$TMUX" ]; then
     return
 fi
 
-TEXAS_RANGER_PID=$(tmux split-window -p 70 -P -F '#{pane_pid}' "TEXAS_SHELL_PID=$$ ranger")
+TEXAS_RANGER_PID=$(tmux split-window -p 70 -P -F '#{pane_pid}' "LAUNCH_TEXAS=$LAUNCH_TEXAS TEXAS_SHELL_PID=$$ ranger")
+
+# Unset the variable only here because the ranger plugin reacts to it.
+unset LAUNCH_TEXAS
 
 autoload -U add-zsh-hook
 

texas.py
@@ -32,12 +32,20 @@ def hook_init(fm):
         # Bind to C-o
         fm.execute_console("""map <C-o> shell -s if [ "$(tmux display-message -p '#{window_panes}')" -gt 1 ]; then tmux select-pane -t :.+; else tmux next-window; fi""")
 
-        # Close the associated shell along with the whole texas on
-        # ranger exit.
-        import atexit
-        def texas_cleanup():
-            os.kill(texas_shell_pid, signal.SIGHUP)
-        atexit.register(texas_cleanup)
+        # If a new tmux session needed to be created i.e. texas wasn't
+        # started from inside of tmux.
+        if int(os.environ['LAUNCH_TEXAS']):
+            # Close the associated shell along with the whole texas on
+            # ranger exit unless it was started inside of an existing
+            # tmux session.
+            import atexit
+            def texas_cleanup():
+                os.kill(texas_shell_pid, signal.SIGHUP)
+            atexit.register(texas_cleanup)
+        # Prevent the child processes from inheriting this env var so
+        # that the spawned shells will not start nested texases
+        # immediately.
+        del os.environ['LAUNCH_TEXAS']
     except KeyError:
         # The texas shell is not running.
         pass

texas_init.zsh
@@ -6,6 +6,5 @@ fi
 
 autoload -U texas
 if [ -n "$LAUNCH_TEXAS" ]; then
-    unset LAUNCH_TEXAS
     texas
 fi

commit ace42b7a617cb1064492a1fc4f8150e256bfb964

the shell's state properly cleaned up after closing ranger
Fixes the bugs introduced in da596f2.

texas.bash
@@ -20,7 +20,15 @@ texas()
     cd()
     {
         builtin cd "$@"
-        kill -USR1 $TEXAS_RANGER_PID
+        if ! kill -USR1 $TEXAS_RANGER_PID 2> /dev/null; then
+            # ranger is no longer running, let's clean up the bash state.
+
+            # The ranger's PID is no longer needed.
+            unset TEXAS_RANGER_PID
+
+            # Remove the hook because there is no ranger to communicate with.
+            unset -f cd
+        fi
     }
 
     texas--exit-cleanup() {

texas.zsh
@@ -14,7 +14,18 @@ unset LAUNCH_TEXAS
 autoload -U add-zsh-hook
 
 texas--sh-to-ranger-sync() {
-    kill -USR1 $TEXAS_RANGER_PID
+    if ! kill -USR1 $TEXAS_RANGER_PID 2> /dev/null; then
+        # ranger is no longer running, let's clean up the zsh state.
+
+        # The ranger's PID is no longer needed.
+        unset TEXAS_RANGER_PID
+
+        # TODO: save the original binding instead of hardcoding the assumed default.
+        bindkey "^o" accept-line-and-down-history
+
+        # Remove the hook because there is no ranger to communicate with.
+        chpwd_functions=${chpwd_functions:#texas--sh-to-ranger-sync}
+    fi
 }
 add-zsh-hook chpwd texas--sh-to-ranger-sync
 